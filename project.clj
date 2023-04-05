da5cd1e20098daddee004a66b8421b74a0d93306
Toby Crawley <toby@tcrawley.org>
2023-04-05 10:59:26 -0400

    Release fork

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject io.rkn/conformity "0.5.4"
+(defproject org.tcrawley/conformity "0.6.0"
   :description "Idempotent datom transacting for Datomic.\n\nSpecial thanks to Stuart Halloway for the original idea, implementation and permission to take it and run."
   :url "http://github.com/rkneufeld/conformity"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
                                   [com.datomic/datomic-free "0.9.4815.12"]
                                   [org.clojure/tools.namespace "0.2.3"]]
                    :source-paths ["dev"]}})
